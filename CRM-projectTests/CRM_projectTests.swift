f71af85acbdfbbe20635934691c3fb187c0b9633
Nikolay <nikolay@huivam.local>
2018-08-14 10:20:15 -0600

    Initial Commit

---
+++ CRM-projectTests/CRM_projectTests.swift
@@ -0,0 +1,34 @@
+//
+//  CRM_projectTests.swift
+//  CRM-projectTests
+//
+//  Created by Nikolay on 8/14/18.
+//  Copyright © 2018 Nikolay. All rights reserved.
+//
+
+import XCTest
+@testable import CRM_project
+
+class CRM_projectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
