5332e01c98db63fb4da6e000180c3937ec71b01f
Nikolay <nikolay@huivam.local>
2018-08-15 12:55:35 -0600

    Added JSON file with basic info

--- CRM-project/ViewControllers/ClientsInfoAPI.swift
+++ CRM-project/ViewControllers/ClientsInfoAPI.swift
@@ -7,22 +7,22 @@ class ClientsInfoAPI {
     // API link where all account info fetched from
     private let url = "https://glacial-bayou-77287.herokuapp.com/listAccounts"
     
-    func fetchData (completion : @escaping ([BankAccountItem]) -> Void) {
+    func fetchData (completion : @escaping ([ClientInfo]) -> Void) {
         
-        // Array which stores all the bank accounts info parsed from JSON
-        var jsonAllResults = [BankAccountItem]()
+        // Array stores all the clients info parsed from JSON
+        var AllResults = [ClientInfo]()
         
         Alamofire.request(url, encoding: JSONEncoding.default).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
                 let results = JSON(value)
                 for index in 0..<results.count {
-                    jsonAllResults.append(BankAccountItem(name: results[index]["name"].stringValue,
+                    AllResults.append(ClientInfo(name: results[index]["name"].stringValue,
                                                           balance: results[index]["amount"].doubleValue,
                                                           ROI: results[index]["ROI"].doubleValue,
                                                           id: results[index]["id"].intValue))
                 }
-                completion(jsonAllResults)
+                completion(AllResults)
             case .failure( _):
                 print("Check your internet connection or url")
             }
